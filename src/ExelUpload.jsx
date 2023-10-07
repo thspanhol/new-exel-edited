2c9be18766994f6ce1a01e83486426af9bd38d26
Thales Spanhol <thales_spanhol@hotmail.com>
2023-10-07 12:08:56 -0300

    get data

--- src/ExelUpload.jsx
+++ src/ExelUpload.jsx
@@ -23,7 +23,7 @@ function ExcelUpload() {
 
     if (array.includes(login)) {
 
-      console.log(array);
+      /* console.log(array); */
 
       let index = array.indexOf(login) +1
 
@@ -31,7 +31,11 @@ function ExcelUpload() {
       console.log(objeto[`B${index}`].v)
       console.log(objeto[`C${index}`].v)
       console.log(objeto[`D${index}`].v)
-      console.log(objeto[`E${index}`].v)
+
+      let date = XLSX.SSF.parse_date_code(objeto[`E${index}`].v)
+      let formattedDate = `${date.d < 10 ? `0${date.d}` : date.d}/${date.m < 10 ? `0${date.m}` : date.m}/${date.y}`;
+     
+      console.log(formattedDate)
       console.log(objeto[`F${index}`].v)
     } else {
       console.log('Usuário não encontrado.');
@@ -49,25 +53,24 @@ function ExcelUpload() {
   
         // Aqui é possível acessar as folhas do arquivo Excel usando workbook.Sheets
         // e fazer qualquer transformação necessária
-        console.log(workbook.Sheets);
+        /* console.log(workbook.Sheets);
         console.log(workbook.Sheets['SIS_USUARIOS']);
         console.log(typeof workbook.Sheets['SIS_USUARIOS']);
         console.log(workbook.Sheets['SIS_USUARIOS'][0]);
-        console.log(typeof workbook.Sheets['SIS_USUARIOS'][0]);
+        console.log(typeof workbook.Sheets['SIS_USUARIOS'][0]); */
         // console.log(workbook.Sheets['NOME DO AQUIVO (mostrado na linha 22)']['A2']);
         
-        console.log(workbook.Sheets['SIS_USUARIOS']['A9'].v );
 
         logObjeto(workbook.Sheets['SIS_USUARIOS'], userParam)
 
        /*  workbook.Sheets['Planilha1']['A2'].v = 'Thales' */
   
         // Por exemplo, criar um novo arquivo Excel:
-        const newWorkbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(newWorkbook, workbook.Sheets[workbook.SheetNames[0]], 'Nova Planilha');
+        /* const newWorkbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(newWorkbook, workbook.Sheets[workbook.SheetNames[0]], 'Nova Planilha'); */
   
         // Agora é possível baixar o novo arquivo Excel
-        XLSX.writeFile(newWorkbook, 'novo_excel.xlsx');
+        /* XLSX.writeFile(newWorkbook, 'novo_excel.xlsx'); */
       };
       reader.readAsBinaryString(excelFile);
     }
@@ -78,9 +81,8 @@ function ExcelUpload() {
     <div>
       <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload Excel</button>
-      <input type='text' value={user} onChange={(e) => setUser(e.value)} onKeyDown={(e) => {
+      <input type='text' value={user} onChange={(e) => setUser(e.target.value)} onKeyDown={(e) => {
         if (e.key === 'Enter') {
-          console.log(user);
           handleUpload(user)
         }
       }} />
