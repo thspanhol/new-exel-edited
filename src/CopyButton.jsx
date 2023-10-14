dd2a35dcd09afeba22ab5dbafef6078096fcf075
Thales Spanhol <thales_spanhol@hotmail.com>
2023-10-14 16:26:10 -0300

    edit copy button

--- src/CopyButton.jsx
+++ src/CopyButton.jsx
@@ -4,17 +4,13 @@ const CopyButton = ({ text }) => {
   const textRef = useRef(null);
 
   const copyText = () => {
-    // Seleciona o texto dentro do elemento do botão
     textRef.current.select();
-    // Copia o texto selecionado para a área de transferência do navegador
     document.execCommand("copy");
-    // Desseleciona o texto para evitar efeitos colaterais de seleção visível
     window.getSelection().removeAllRanges();
   };
 
   return (
     <div>
-      {/* Usa um elemento input do tipo "text" para armazenar o texto que será copiado */}
       <input
         type="text"
         value={text}
@@ -22,8 +18,9 @@ const CopyButton = ({ text }) => {
         style={{ position: "absolute", left: "-9999px" }}
         readOnly
       />
-      {/* O botão que ao ser clicado irá copiar o texto */}
-      <button className="btn" onClick={copyText}>{text}</button>
+      <button className="btn" onClick={copyText}>
+        {text}
+      </button>
     </div>
   );
 };
